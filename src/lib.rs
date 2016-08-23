a25ee0c4a63e911c8aa70d88baebb0246551a8ea
Sgeo <sgeoster@gmail.com>
2016-08-23 19:38:49 -0400

    Compiles using 32-bit Rust, x1.exe chokes on aw_say not existing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#![allow(non_snake_case)]
+
+extern crate libc;
+
+#[no_mangle]
+pub extern fn aw_init(version: libc::c_int) -> libc::c_int {
+    0
+}
+
+#[allow(dead_code)]
+fn spare() { println!(""); } //adding this (doesn't have to be named "spare") makes the compilation work. https://github.com/rust-lang/rust/issues/18807
+
 #[cfg(test)]
 mod tests {
     #[test]

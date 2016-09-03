e80dc5806cb3dfe8ed07c8c94d4b6a33946391a7
Sgeo <sgeoster@gmail.com>
2016-09-03 01:14:54 -0400

    Default to VP universe if nothing given.

--- src/instance.rs
+++ src/instance.rs
@@ -1,4 +1,5 @@
 use std::os::raw::{c_int, c_char, c_void};
+use std::ffi::CStr;
 
 use raw::{aw, vp};
 
@@ -31,10 +32,16 @@ pub extern fn aw_create(domain: *const c_char, port: c_int, instance: *mut *mut 
     let vp;
     let result;
     unsafe {
+        let dest_domain = if domain.is_null() {
+            CStr::from_bytes_with_nul_unchecked(b"universe.virtualparadise.org\0").as_ptr()
+        } else {
+            domain
+        };
+        let dest_port = if port == 0 { 57000 } else { port };
         vp = vp::create();
         debug!("vp pointer: {:?}", vp);
         *instance = vp;
-        result = vp::connect_universe(vp, domain, port);
+        result = vp::connect_universe(vp, dest_domain, dest_port);
     }
     let instance = Instance { vp: vp, attributes: AttribBuffer::new() };
     let mut globals = GLOBALS.lock().unwrap();
